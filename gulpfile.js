d7093bc1ead854f3b7fbbefb655e2441b36d0837
Mihó Dániel <dev.mihodaniel@gmail.com>
2019-05-17 17:13:01 +0200

    Remake portfolio

--- gulpfile.js
+++ gulpfile.js
@@ -1,104 +1,29 @@
-var gulp = require('gulp');
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
 
-// Dependencies for gulp
-var sass         = require('gulp-sass');
-var browserSync  = require('browser-sync').create();
-var useref       = require('gulp-useref');
-var uglify       = require('gulp-uglify');
-var gulpIf       = require('gulp-if');
-var imagenin     = require('gulp-imagemin');
-var cssnano      = require('gulp-cssnano');
-var cache        = require('gulp-cache');
-var del          = require('del');
-var runSequence  = require('run-sequence');
+function reload(done) {
+    browserSync.reload();
+    done();
+}
 
-// Supported browsers
-var supported = [
-  'last 2 versions',
-  'safari >= 8',
-  'ie >= 10',
-  'ff >= 20',
-  'ios 6',
-  'android 4'
-];
+function style() {
+    return (
+        gulp.src('app/scss/styles.scss')
+            .pipe(sass())
+            .pipe(gulp.dest('app/css'))
+            .pipe(browserSync.stream())
+    );
+}
 
-// MUST HAVE
-// ---------
-// - Live reloading
-// - SCSS compiling
-gulp.task('browserSync', function () {
-  browserSync.init({
-    proxy: 'localhost/portfolio/app'
-  });
-});
+function watch() {
+    browserSync.init({
+        // proxy: 'mihodaniel.local/app/'
+        watch: true,
+        server: "./app"
+    });
+    gulp.watch("app/scss/**/*.scss", style);
+    gulp.watch("app/*.html", reload);
+}
 
-gulp.task('sass', function () {
-  return gulp.src('app/scss/**/*.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('app/css'))
-    .pipe(browserSync.reload({
-      stream: true
-    }))
-});
-
-// WATCHING
-// --------
-gulp.task('watch', function () {
-  gulp.watch('app/scss/**/*.scss', ['sass']);
-  // Reload when html or js file changes too
-  gulp.watch('app/**/*.html', browserSync.reload);
-  gulp.watch('app/**/*.php', browserSync.reload);
-  gulp.watch('app/js/**/*.js', browserSync.reload);
-});
-
-
-// Optimizing JS and CSS
-gulp.task('useref', function () {
-  return gulp.src('app/**/*.html')
-    .pipe(useref())
-    // Minifies if it's a JavaScript file
-    .pipe(gulpIf('*.js', uglify()))
-    // Minifies if it's a CSS file
-    .pipe(gulpIf('*.css', cssnano({
-      autoprefixer: { browsers: supported, add: true }
-    })))
-    .pipe(gulp.dest('dist'))
-});
-
-// Optimizing images
-gulp.task('images', function () {
-  return gulp.src('app/img/**/*.+(png|jpg|gif|svg)')
-    .pipe(cache(imagenin({
-      interlaced: true
-    })))
-    .pipe(gulp.dest('dist/img'))
-});
-
-// CLEANING UP
-// -----------
-gulp.task('clean', function () {
-  return del.sync('dist').then(function(cb) {
-    return cache.clearAll(cb);
-  });
-})
-
-gulp.task('clean:dist', function () {
-  return del.sync(['dist/**/*', '!dist/images', '!dist/images/**/*']);
-});
-
-// BUILDING
-// --------
-gulp.task('default', function (callback) {
-  runSequence(['sass', 'browserSync'], 'watch',
-    callback
-  )
-});
-
-gulp.task('build', function (callback) {
-  runSequence(
-    'clean:dist',
-    'sass',
-    ['useref', 'images'],
-    callback
-  )
-});
+exports.watch = watch;
